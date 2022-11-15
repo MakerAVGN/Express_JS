bcc2c9c39b6e3072540afe3c7cb473fe47938c35
MakerAVGN <ivan.shapkin.2015@mail.ru>
2022-11-15 23:24:45 +0300

    добавил сферы

--- invoker/routes/index.js
+++ invoker/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/quas', function(req, res, next) {
+  res.send("<h1>QUAS</h1>");
+});
+router.get('/wex', function(req, res, next) {
+  res.send("<h1>WEX</h1>");
+});
+router.get('/exort', function(req, res, next) {
+  res.send("<h1>EXORT</h1>");
 });
 
 module.exports = router;
