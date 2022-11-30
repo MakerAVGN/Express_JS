e7ef01677700fb0b4a08d5ad6c7cc21abecbfc73
MakerAVGN <ivan.shapkin.2015@mail.ru>
2022-12-01 00:24:51 +0300

    Обработка параметра в адресе

--- invoker/app.js
+++ invoker/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/invoker')
+mongoose.connect('mongodb://127.0.0.1:27017/invoker')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- invoker/mongooseTasks.js
+++ invoker/mongooseTasks.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test1')
-var Inv = require("./models/invoker").Inv
+var Inv = require("../models/invoker").Inv
 
 
 var inv = new Inv({
