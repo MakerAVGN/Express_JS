bf272adc4e1b80ef04854ecf5b717cd2ac070eb2
MakerAVGN <ivan.shapkin.2015@mail.ru>
2022-12-29 00:48:50 +0300

    Обработка ошибки аутентификации

--- invoker/routes/index.js
+++ invoker/routes/index.js
@@ -1,40 +1,48 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var Inv = require("../models/invoker").Inv
+var User = require("./../models/user").User
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express', menu:menu, counter:req.session.counter });
+    Inv.find({},{_id:0,title:1,nick:1},function(err,menu){
+        req.session.greeting = "Hi!!!",
+            res.cookie('greeting', 'Hi!!!').render('index', {
+            title: 'Express',
+            menu: menu,
+            counter:req.session.counter
+        });
+    })
+  
 });
-
-module.exports = router;
-
+  
 /* GET login/registration page. */
-router.get('/logreg', function(req, res, next) {
-res.render('logreg',{title: 'Вход'});
-});
-
-   /* POST login/registration page. */
-router.post('/logreg', function(req, res, next) {
+router.get('/logreg', function (req, res, next) {
+    res.render('logreg', { title: 'Вход', error: null });
+  });
+  
+  
+  /* POST login/registration page. */
+  router.post('/logreg', function (req, res, next) {
     var username = req.body.username
     var password = req.body.password
-  User.findOne({username:username},function(err,user){
-        if(err) return next(err)
-        if(user){
-            if(user.checkPassword(password)){
-                req.session.user = user._id
-                res.redirect('/')
-            } else {
-                      res.render('logreg', {title: 'Вход'})
-            }
+    User.findOne({ username: username }, function (err, user) {
+      if (err) return next(err)
+      if (user) {
+        if (user.checkPassword(password)) {
+          req.session.user = user._id
+          res.redirect('/')
         } else {
-            var user = new User({username:username,password:password})
-            user.save(function(err,user){
-                if(err) return next(err)
-                req.session.user = user._id
-                res.redirect('/')
-            })        
+          res.render('logreg', { title: 'Вход', error: "Пароль не верный" })
+        }
+      } else {
+        var user = new User({ username: username, password: password })
+        user.save(function (err, user) {
+          if (err) return next(err)
+          req.session.user = user._id
+          res.redirect('/')
+        })
       }
     })
   });
