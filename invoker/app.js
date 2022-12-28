5c83462cf593dc0be69cc81144df28ae393a5980
MakerAVGN <ivan.shapkin.2015@mail.ru>
2022-12-28 23:56:28 +0300

    Создание счетчика посещения страниц

--- invoker/app.js
+++ invoker/app.js
@@ -32,6 +32,11 @@ app.use(session({
     saveUninitialized: true,
     store: MongoStore.create({mongoUrl: 'mongodb://localhost/invoker'})
 }))
+app.use(function(req,res,next){
+    req.session.counter = req.session.counter +1 || 1
+    next()
+})
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- invoker/routes/index.js
+++ invoker/routes/index.js
@@ -5,13 +5,8 @@ var Inv = require("../models/invoker").Inv
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Inv.find({},{_id:0,title:1,nick:1},function(err,menu){
-    req.session.greeting = "Hi!!!",
-    render('index',  { 
-                            title: 'Express',
-                            menu: menu
-                        });
+    res.render('index', { title: 'Express', menu:menu, counter:req.session.counter });
 });
-});
+
 module.exports = router;
 
