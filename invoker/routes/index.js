ccc36c43dfd3fb53fd49a7e4700ff130822c082f
MakerAVGN <ivan.shapkin.2015@mail.ru>
2022-12-29 00:33:58 +0300

    Подготовка данных для передачи на сервер

--- invoker/routes/index.js
+++ invoker/routes/index.js
@@ -14,3 +14,9 @@ module.exports = router;
 router.get('/logreg', function(req, res, next) {
 res.render('logreg',{title: 'Вход'});
 });
+
+/* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+var password = req.body.password
+});
